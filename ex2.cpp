ea9d1fff918400096d73c1c14b0f47794ae33283
ahmedroshdy <ahmedroshdy2013@hotmail.com>
2015-10-20 06:14:14 +0200

    Exercise 2

---
+++ ex2.cpp
@@ -0,0 +1,22 @@
+
+#include"Person.h"
+
+void main(){
+	Person one; // use default constructor
+	Person two("Smythecraft"); // use #2 with one default argument
+	Person three("Dimwiddy", "Sam"); // use #2, no defaults
+		one.Show();
+	cout << endl;
+	one.FormalShow();
+	cout << endl;
+	two.Show();
+	cout << endl;
+	two.FormalShow();
+	cout << endl;
+	three.Show();
+	cout << endl;
+	three.FormalShow();
+	cout << endl;
+	
+
+}
